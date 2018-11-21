287337cc735b49f130d8ebfbab1ffd6a93351251
ericojonathan <ericojonathan@hotmail.com>
2018-11-21 19:38:24 +0700

    Added bulma and modified index.php

--- plain_vanilla_php/index.php
+++ plain_vanilla_php/index.php
@@ -3,9 +3,14 @@
 	<head>
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" href="/assets/css/bulma.min.css">
 		<title>Plain Vanilla PHP</title>
 	</head>
 	<body>
-		<h1>Plain Vanilla PHP</h1>		
+		<div class="hero">
+			<div class="hero-body">
+				<h1>Plain Vanilla PHP</h1>		
+			</div>
+		</div>
 	</body>
 </html>
