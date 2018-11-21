1de03e19ec427e10a86a181db0760758c2eadcca
ericojonathan <ericojonathan@hotmail.com>
2018-11-21 20:16:33 +0700

    moved the files to erics_acme_shop

---
+++ plain_vanilla_php/erics_acme_shop/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" href="/assets/css/bulma.min.css">
+		<title>Eric's ACME Shop</title>
+	</head>
+	<body>
+		<div class="hero">
+			<div class="hero-body">
+				<h1 class="title">Eric's ACME Shop</h1>		
+				<h2 class="subtitle">"Where Small Things Count"</h2>
+			</div>
+			<div class="container">
+				<p>Welcome to Eric's ACME shop, where you can find nice small things to buy that are perfect for you or as gifts for your loved ones and friends.</p>
+				<p>Have fun shopping with us!</p>
+				<div id="sample-products">
+
+				</div>
+			</div>
+		</div>
+	</body>
+</html>
