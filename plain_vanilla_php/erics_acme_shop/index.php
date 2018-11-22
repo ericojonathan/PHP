5d0e21314e4d0ab186854ae6a82b5e799854a32a
git-php <ericojonathan@hotmail.com>
2018-11-22 11:37:12 +0700

    modified index, header and added google icon

--- plain_vanilla_php/erics_acme_shop/index.php
+++ plain_vanilla_php/erics_acme_shop/index.php
@@ -1,6 +1,14 @@
 <?php
 	$PageTitle = "Eric's ACME Shop";
 ?>
+
+<style>
+ul li:before {
+	font-family: "Webdings";
+	content: "= ";
+}
+</style>
+
 <script>
 function showLogin(show){
 	if(show == true){
@@ -19,6 +27,7 @@ function showSignUp(show){
 }
 
 </script>
+
 <?php include_once("header.php"); ?>
 	<div id="login" class="box" style="float:right;font-size:14px;margin:15px;width:165px;max-height:60px">
 		<div class="columns">
@@ -34,12 +43,15 @@ function showSignUp(show){
 			<div class="modal-background"></div>
 			<div class="modal-card" style="max-width:550px">
 				<header class="modal-card-head">
-					<p class="modal-card-title" style="color:indianred;font-weight:bold">Login</p>
+					<p class="modal-card-title" style="color:green;font-weight:bold">Eric's ACME Shop</p>
 					<button class="delete" aria-label="close" onclick="showLogin(false)"></button>
 				</header>
 				<section class="modal-card-body">
-					<input id="login_username_txt" class="input" type="text" placeholder="username" style="margin:15px;max-width:475px">
-					<input id="login_password_txt" class="input" type="password" placeholder="password" style="margin:15px;max-width:475px">
+								<h1 class="title is-5" style="margin-left:15px">Login</h1>
+								<p style="font-weight:bold;margin-left:15px">User Name</p>
+								<input id="login_username_txt" class="input" type="text" placeholder="username" style="margin:15px;max-width:475px">
+								<p style="font-weight:bold;margin-left:15px">Password</p>
+								<input id="login_password_txt" class="input" type="password" placeholder="password" style="margin:15px;max-width:475px">
 				</section>
 				<footer class="modal-card-foot" style="justify-content:flex-end">
 					<button id="cancel_btn" class="button" onclick="showLogin(false)">Cancel</button>
@@ -50,23 +62,36 @@ function showSignUp(show){
 
 		<div id="modal-signup" class="modal">
 			<div class="modal-background"></div>
-			<div class="modal-card" style="max-width:550px">
+			<div class="modal-card">
 				<header class="modal-card-head">
-					<p class="modal-card-title" style="color:green;font-weight:bold">Sign Up</p>
+					<p class="modal-card-title" style="color:green;font-weight:bold">Eric's ACME Shop</p>
 					<button class="delete" aria-label="close" onclick="showSignUp(false)"></button>
 				</header>
 				<section class="modal-card-body">
-					<input id="signup_email_txt" class="input" type="text" placeholder="E-Mail" style="margin:15px;max-width:475px">
-					<input id="signup_username_txt" class="input" type="text" placeholder="username" style="margin:15px;max-width:475px">
-					<input id="signup_password_txt" class="input" type="password" placeholder="password" style="margin:15px;max-width:475px">
-				</section>
+					<div class="columns">
+						<div class="column">
+							<h1 class="title is-5">Sign Up using:</h1>
+							<ul>
+								<li class="title is-5"><i class="fab fa-facebook" style="color:rgba(59,89,152)"></i> Facebook</li>
+								<li class="title is-5"><i class="fab fa-twitter-square" style="color:rgba(0,132,180)"></i> Twitter</li>
+								<li class="title is-5"><img src="/assets/images/google_g_icon.png" style="height:20px;width:20px"></img> Google</li>
+							</ul>
+						</div>
+						<div class="column is-1" style="border-left:2px solid lightgray"></div>
+						<div class="column" style="margin-right:20px">
+							<h1i class="title is-5" style="margin-left:15px">Or by Email:</h1>
+							<input id="signup_email_txt" class="input" type="text" placeholder="E-Mail" style="margin:15px;max-width:475px">
+							<input id="signup_username_txt" class="input" type="text" placeholder="username" style="margin:15px;max-width:475px">
+							<input id="signup_password_txt" class="input" type="password" placeholder="password" style="margin:15px;max-width:475px">
+						</div>
+					</div>
+									</section>
 				<footer class="modal-card-foot" style="justify-content:flex-end">
 					<button id="cancel_btn" class="button" onclick="showSignUp(false)">Cancel</button>
 					<button id="login_btn"  class="button is-primary">Sign Up</button>
 				</footer>
 			</div>
 		</div>
-
 
 	</div>
 	<div class="hero">
